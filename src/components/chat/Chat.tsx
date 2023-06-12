2800b845b168995461c7830a370b0b2b45da65fa
Tomoko-hub <haisaitomoco@gmail.com>
2023-06-12 14:38:50 +0300

    create inputText funtionality using firebase snapshot

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "./Chat.scss"
 import ChatHeader from './ChatHeader';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -7,10 +7,67 @@ import GifIcon from '@mui/icons-material/Gif';
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
 import ChatMessage from './ChatMessage';
 import { useAppSelector } from '../../app/hooks';
+import { CollectionReference, DocumentData, addDoc, collection, serverTimestamp, DocumentReference, onSnapshot, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebase';
 
-const Home = () => {
+interface Messages {
+  timestamp:Timestamp;
+  message:string;
+  user:{
+    uid: string;
+        photo: string;
+        email: string;
+        displayName: string;
+  };
+}
+
+const Chat = () => {
+  const [inputText, setInputText ] = useState<string>("");
+  const [messages, setMessages ] = useState<Messages[]>([]);
   const channelName = useAppSelector((state)=>state.channel.channelName);
-  //console.log(channelName);
+  const channelId = useAppSelector((state)=>state.channel.channelId);
+  const user = useAppSelector((state)=>state.user.user);
+
+  useEffect(()=>{
+
+    let collectionRef = collection(
+      db,
+      "channels", 
+      String(channelId),
+      "messages"
+      );
+      //firebaseリアルタイムでデータベースを更新するときはonSnapshot
+    onSnapshot(collectionRef,(snapshot)=>{
+      let results:Messages[]=[];
+      snapshot.docs.forEach((doc)=>{
+        results.push({
+          message:doc.data().message,
+          timestamp:doc.data().timestamp,
+          user:doc.data().user,
+        });
+      });
+      setMessages(results);
+      console.log(results);
+    });
+  },[channelId]);
+
+  const sendMessage = async(event:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+    event.preventDefault();
+    //firebase data
+    const collectionRef:CollectionReference<DocumentData>= collection(
+      db, 
+      "channels", 
+      String(channelId), 
+      "messages");
+
+      const docRef:DocumentReference<DocumentData>=await addDoc(collectionRef, {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      });
+
+      console.log(docRef);
+  }
 
   return (
     <div className='chat'>
@@ -32,10 +89,14 @@ const Home = () => {
           <input 
             type="text"
             placeholder='#Send message to Tomoko'
+            onChange={(event:React.ChangeEvent<HTMLInputElement>)=>setInputText(event.target.value)}
            />
           <button 
             type="submit"
-            className='chatInputButton'  
+            className='chatInputButton'
+            onClick={(event:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+              sendMessage(event)
+            }}
           >Send</button>
         </form>
         <div className="chatInputIcons">
@@ -48,4 +109,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Chat
